59492ba882f5eebb82086242addbedc2454925c1
Ashpak <ash7@Ashpaks-MacBook-Air.local>
2023-05-22 15:20:04 +0530

    added meta in index

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+<meta
+  http-equiv="Content-Security-Policy"
+  content="upgrade-insecure-requests"
+/>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
