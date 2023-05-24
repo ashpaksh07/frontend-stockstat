baae939a21714b4fbdad07e576736fd5e9a74e7a
Ashpak <ash7@Ashpaks-MacBook-Air.local>
2023-05-24 18:54:08 +0530

    responsive changes for other pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,58 +6,114 @@ import AboutUsImage from "../images/about-us.png";
 const About = () => {
   return (
     <Layout>
-      <Box
-        sx={{
-          mr: 9,
-          ml: 8,
-          textAlign: "center",
+      <Box sx={{ display: { xs: "none", sm: "block" }, mr: 5 }}>
+        <Box
+          sx={{
+            mr: 9,
+            ml: 8,
+            textAlign: "center",
 
-          "& h4": {
-            fontWeight: "bold",
-            my: 2,
-            fontSize: "2rem",
-          },
-          "& p": {
-            textAlign: "justify",
-          },
-          "@media (max-width:600px)": {
-            mt: 0,
             "& h4": {
-              fontSize: "1.3rem",
+              fontWeight: "bold",
+              my: 2,
+              fontSize: "2rem",
             },
-          },
-        }}
-      >
-        <Grid container>
-          <Grid item xs={6}>
-            <Box color="#505050" marginTop="180px">
-              <Typography variant="h4">About us</Typography>
-              <p>
-                We at StockStat analyse your pnl book and simplify it so that
-                users can understand how much money they have added in Zerodha
-                till now. Zerodha will show you only current value and currently
-                invested amount. But, we might have done profit and loss.
-                Zerodha ignored this profit and loss by showing just current
-                investment. But do not worry, StockStat is there for your help.
-              </p>{" "}
-              <br />
-              <p>
-                StockStat will take the csv file and analyse it to find out
-                total money you have added in Zerodha? How much charges and what
-                kind of charges Zerodha have deducted from you? And How much
-                profit or loss you have done in Futures and Options. Please let
-                us know if you have any feedback for this site on
-                ashpaksh07@gmail.com.
-              </p>
-            </Box>
+            "& p": {
+              textAlign: "justify",
+            },
+            "@media (max-width:600px)": {
+              mt: 0,
+              "& h4": {
+                fontSize: "1.3rem",
+              },
+            },
+          }}
+        >
+          <Grid container>
+            <Grid item xs={6}>
+              <Box color="#505050" marginTop="180px">
+                <Typography variant="h4">About us</Typography>
+                <p>
+                  We at StockStat analyse your pnl book and simplify it so that
+                  users can understand how much money they have added in Zerodha
+                  till now. Zerodha will show you only current value and
+                  currently invested amount. But, we might have done profit and
+                  loss. Zerodha ignored this profit and loss by showing just
+                  current investment. But do not worry, StockStat is there for
+                  your help.
+                </p>{" "}
+                <br />
+                <p>
+                  StockStat will take the csv file and analyse it to find out
+                  total money you have added in Zerodha? How much charges and
+                  what kind of charges Zerodha have deducted from you? And How
+                  much profit or loss you have done in Futures and Options.
+                  Please let us know if you have any feedback for this site on
+                  ashpaksh07@gmail.com.
+                </p>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <div
+                className="aboutusimage"
+                style={{ backgroundImage: `url(${AboutUsImage})` }}
+              ></div>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <div
-              className="aboutusimage"
-              style={{ backgroundImage: `url(${AboutUsImage})` }}
-            ></div>
+        </Box>
+      </Box>
+      {/* <<<<<<<<<<<<< mobile view starts here >>>>>>>>>>>>>> */}
+      <Box sx={{ display: { xs: "block", sm: "none" }, mr: 1 }}>
+        <Box
+          sx={{
+            mr: 2,
+            ml: 2,
+            mb: 7,
+            textAlign: "center",
+
+            "& h4": {
+              fontWeight: "bold",
+              my: 2,
+              fontSize: "22px",
+            },
+            "& p": {
+              textAlign: "justify",
+              fontSize: "15px",
+            },
+          }}
+        >
+          <Grid container>
+            <Grid item xs={12}>
+              <div
+                className="aboutusimage"
+                style={{ backgroundImage: `url(${AboutUsImage})` }}
+              ></div>
+            </Grid>
+            <Grid item xs={12}>
+              <Box color="#505050" marginTop="10px">
+                <Typography variant="h4">About us</Typography>
+                <p>
+                  We at StockStat analyse your pnl book and simplify it so that
+                  users can understand how much money they have added in Zerodha
+                  till now. Zerodha will show you only current value and
+                  currently invested amount. But, we might have done profit and
+                  loss. Zerodha ignored this profit and loss by showing just
+                  current investment. But do not worry, StockStat is there for
+                  your help.
+                </p>{" "}
+                <br />
+                <p>
+                  StockStat will take the csv file and analyse it to find out
+                  total money you have added in Zerodha? How much charges and
+                  what kind of charges Zerodha have deducted from you? And How
+                  much profit or loss you have done in Futures and Options.
+                  Please let us know if you have any feedback for this site on
+                  ashpaksh07@gmail.com.
+                </p>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Box>
     </Layout>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -20,77 +20,144 @@ import ContactImage from "../images/contact.png";
 const Contact = () => {
   return (
     <Layout>
-      <Grid container>
-        <Grid item xs={6}>
-          <div
-            className="contactimage"
-            style={{ backgroundImage: `url(${ContactImage})` }}
-          ></div>
+      <Box sx={{ display: { xs: "none", sm: "block" }, mr: 1 }}>
+        <Grid container>
+          <Grid item xs={6}>
+            <div
+              className="contactimage"
+              style={{ backgroundImage: `url(${ContactImage})` }}
+            ></div>
+          </Grid>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                mt: 17,
+                mb: 4,
+                ml: 10,
+                "& h5": {
+                  fontWeight: "bold",
+                  mb: 2,
+                },
+              }}
+            >
+              <Typography variant="h5" color="#505050">
+                Contact Us
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                ml: 10,
+                mr: 10,
+              }}
+            >
+              <TableContainer component={Paper}>
+                <Table aria-label="contact table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell
+                        sx={{ bgcolor: "#505050", color: "white" }}
+                        align="center"
+                      >
+                        Contact Details
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <EmailIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
+                        ashpaksh7@gmail.com
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <InstagramIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
+                        @iam_ashpak
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <SupportAgentIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
+                        ashpaksh7@gmail.com
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Box
-            sx={{
-              mt: 17,
-              mb: 4,
-              ml: 10,
-              "& h5": {
-                fontWeight: "bold",
-                mb: 2,
-              },
-            }}
-          >
-            <Typography variant="h5" color="#505050">
-              Contact Us
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              ml: 10,
-              mr: 10,
-              "@media (max-width:600px)": {
-                ml: 5,
-                mr: 5,
-                width: "250px",
-              },
-            }}
-          >
-            <TableContainer component={Paper}>
-              <Table aria-label="contact table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell
-                      sx={{ bgcolor: "#505050", color: "white" }}
-                      align="center"
-                    >
-                      Contact Details
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <EmailIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
-                      ashpaksh7@gmail.com
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <InstagramIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
-                      @iam_ashpak
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <SupportAgentIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
-                      ashpaksh7@gmail.com
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Box>
+      </Box>
+      {/* <<<<<<<<<<<<< mobile view starts here >>>>>>>>>>>>>> */}
+      <Box sx={{ display: { xs: "block", sm: "none" } }}>
+        <Grid container>
+          <Grid item xs={12}>
+            <div
+              className="contactimage"
+              style={{ backgroundImage: `url(${ContactImage})` }}
+            ></div>
+          </Grid>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                "& h5": {
+                  fontWeight: "bold",
+                  mb: 4,
+                  justifyContent: "center",
+                  textAlign: "center",
+                },
+              }}
+            >
+              <Typography variant="h5" color="#505050">
+                Contact Us
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                ml: 2,
+                mr: 2,
+                width: "90%",
+                mb: "50px",
+              }}
+            >
+              <TableContainer component={Paper}>
+                <Table aria-label="contact table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell
+                        sx={{ bgcolor: "#505050", color: "white" }}
+                        align="center"
+                      >
+                        Contact Details
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <EmailIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
+                        ashpaksh7@gmail.com
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <InstagramIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
+                        @iam_ashpak
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <SupportAgentIcon sx={{ color: "#298BEA", pt: 1 }} />{" "}
+                        ashpaksh7@gmail.com
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Layout>
   );
 };
